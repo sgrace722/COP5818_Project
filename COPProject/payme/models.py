143ee21b389e996de65bb8757e701226de674167
sgrace722 <145815410+sgrace722@users.noreply.github.com>
2023-10-02 22:10:26 -0400

    created-login-database
    Created login database and barebones for the view and soon form.

--- COPProject/payme/models.py
+++ COPProject/payme/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib import auth
 # Create your models here.
+
+## UNSURE ABOUT model below, because we have not got that far in the chapters...
+## But I will save it here for now LOL
+
+# class LOGIN(models.Model):
+#     user_name = models.CharField(
+#         max_length=50, help_text="The users username to log into the site")
+#     user_password = models.CharField(
+#         max_length=50, help_text="The users password to log into the site")
+#     user = models.ForeignKey(
+#         auth.get_user_model(), on_delete=models.CASCADE)
+
+
+class LOGIN(models.Model):
+    user_name = models.CharField(
+        max_length=50, help_text="The users username to log into the site")
+    user_password = models.CharField(
+        max_length=50, help_text="The users password to log into the site")
+
--- COPProject/payme/views.py
+++ COPProject/payme/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import LOGIN
 # Create your views here.
+
+def login_page(request):
