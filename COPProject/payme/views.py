697188d75c55fec4c835dcfe2a809874cf6fb0d9
sgrace722 <145815410+sgrace722@users.noreply.github.com>
2023-10-05 21:34:31 -0400

    made changes

--- COPProject/payme/views.py
+++ COPProject/payme/views.py
@@ -8,4 +8,12 @@ form = LoginForm()
 
 
 def login_page(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user_name_input = form.cleaned_data[user_name_input]
+            user_password_input = form.cleaned_data[user_password_input]
     return render(request, "login_screen.html", {"method": request.method, "form": form})
+
+# referenced for additional help
+# https://openclassrooms.com/en/courses/7107341-intermediate-django/7263317-create-a-login-page-with-a-function-based-view
