043119920908557f0570d14565633a936fe1c272
William Mollenkopf III <12564813+wmollenkopf@users.noreply.github.com>
2023-11-07 18:57:20 -0500

    fixed the root url to point to the homepage

--- COPProject/COPProject/urls.py
+++ COPProject/COPProject/urls.py
@@ -22,6 +22,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('accounts/', include('allauth.urls')),
+	path('', TemplateView.as_view(template_name='dashboard/home.html'), name='root'),  # Add this line
     path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
+	path('profile/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('', include('payme.urls')),  # Define a root URL pattern
 ]
