f87d01d3aa964b897d14d9940a66e7130f81e8e4
William Mollenkopf III <12564813+wmollenkopf@users.noreply.github.com>
2023-11-07 19:14:28 -0500

    fixed profile url

--- COPProject/COPProject/urls.py
+++ COPProject/COPProject/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 	path('accounts/', include('allauth.urls')),
 	path('', TemplateView.as_view(template_name='dashboard/home.html'), name='root'),  # Add this line
     path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
-	path('profile/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
+	# path('profile/', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('', include('payme.urls')),  # Define a root URL pattern
 ]
